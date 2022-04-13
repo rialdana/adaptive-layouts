93130cce07caa24474ecb747b76899b02e82007a
Ricardo Cardona <emy030697@gmail.com>
2022-04-13 15:01:50 -0600

    :lipstick: Add labels to navigation rail and bottom navigation items

--- app/src/main/java/com/example/adaptivelayouts/ui/components/AppBottomNav.kt
+++ app/src/main/java/com/example/adaptivelayouts/ui/components/AppBottomNav.kt
@@ -2,7 +2,9 @@ package com.example.adaptivelayouts.ui.components
 
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import com.example.adaptivelayouts.ui.navigation.AdaptiveLayoutsDestinations
 
 @Composable
@@ -21,19 +23,22 @@ fun AppBottomNav(
         BottomNavigationItem(
             selected = currentRoute == homeDestination.route,
             onClick = navigateToHome,
-            icon = { NavigationIcon(destination = homeDestination) }
+            icon = { NavigationIcon(destination = homeDestination) },
+            label = { Text(text = stringResource(id = homeDestination.name)) },
         )
 
         BottomNavigationItem(
             selected = currentRoute == searchDestination.route,
             onClick = navigateToSearch,
-            icon = { NavigationIcon(destination = searchDestination) }
+            icon = { NavigationIcon(destination = searchDestination) },
+            label = { Text(text = stringResource(id = searchDestination.name)) },
         )
 
         BottomNavigationItem(
             selected = currentRoute == favoritesDestination.route,
             onClick = navigateToFavorites,
-            icon = { NavigationIcon(destination = favoritesDestination) }
+            icon = { NavigationIcon(destination = favoritesDestination) },
+            label = { Text(text = stringResource(id = favoritesDestination.name)) },
         )
     }
 }
