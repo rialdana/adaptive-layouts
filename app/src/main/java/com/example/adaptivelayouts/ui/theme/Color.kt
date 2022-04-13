0e07c5e7e9acc6412038913b4f452015f11dc281
Ricardo Cardona <emy030697@gmail.com>
2022-04-13 07:54:47 -0600

    :lipstick: Change app colors

--- app/src/main/java/com/example/adaptivelayouts/ui/theme/Color.kt
+++ app/src/main/java/com/example/adaptivelayouts/ui/theme/Color.kt
@@ -2,7 +2,24 @@ package com.example.adaptivelayouts.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val md_theme_light_primary = Color(0xFF006590)
+val md_theme_light_onPrimary = Color(0xFFffffff)
+val md_theme_light_secondary = Color(0xFF0756ce)
+val md_theme_light_onSecondary = Color(0xFFffffff)
+val md_theme_light_error = Color(0xFFB3261E)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFfbfcff)
+val md_theme_light_onBackground = Color(0xFF191c1e)
+val md_theme_light_surface = Color(0xFFfbfcff)
+val md_theme_light_onSurface = Color(0xFF191c1e)
+
+val md_theme_dark_primary = Color(0xFF82cfff)
+val md_theme_dark_onPrimary = Color(0xFF00344d)
+val md_theme_dark_secondary = Color(0xFFb0c5ff)
+val md_theme_dark_onSecondary = Color(0xFF002b76)
+val md_theme_dark_error = Color(0xFFF2B8B5)
+val md_theme_dark_onError = Color(0xFF601410)
+val md_theme_dark_background = Color(0xFF191c1e)
+val md_theme_dark_onBackground = Color(0xFFe1e2e5)
+val md_theme_dark_surface = Color(0xFF191c1e)
+val md_theme_dark_onSurface = Color(0xFFe1e2e5)
